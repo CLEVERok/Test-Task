b8c9a19dd32a4579b21bbfad95ced3dfb62808d1
CLEVERok <93844958+CLEVERok@users.noreply.github.com>
2023-06-07 00:28:13 +0300

    main function
    main function: swap, answer, col etc...

---
+++ index.js
@@ -0,0 +1,105 @@
+const quizData = [
+    {
+        question: "How many planets are in the solar system?",
+        a: "8",
+        b: "9",
+        c: "10",
+        correct: "a",
+    },
+    {
+        question: "WWhat is the freezing point of water?",
+        a: "0",
+        b: "-5",
+        c: "6",
+        correct: "a",
+    },
+    {
+        question: "What is the longest river in the world?",
+        a: "Nile",
+        b: "Amazon",
+        c: "Yangtze",
+        correct: "b",
+    },
+    {
+        question: "How many chromosomes are in the human genome?",
+        a: "42",
+        b: "44",
+        c: "46",
+        correct: "c",
+    },
+    {
+        question: "What is the capital of Canada?",
+        a: "Toronto",
+        b: "Ottawa",
+        c: "Vancouver",
+        correct: "b",
+    },
+    {
+        question: "What is the Jewish New Year called?",
+        a: "Hanukkah",
+        b: "Yom Kippur",
+        c: "Kwanzaa",
+        correct: "a",
+    },
+];
+
+const quiz = document.getElementById('quiz');
+const answerElements = document.querySelectorAll('.answer');
+const questionElement = document.getElementById('question');
+const a_text = document.getElementById('a_text');
+const b_text = document.getElementById('b_text');
+const c_text = document.getElementById('c_text');
+const submit = document.getElementById('submit');
+
+let currentQuiz = 0;
+let score = 0;
+
+loadQuiz();
+
+function loadQuiz(){
+    deselectAnswers();
+
+    const currentQuizData = quizData[currentQuiz];
+
+    questionElement.innerText = currentQuizData.question;
+    a_text.innerText = currentQuizData.a;
+    b_text.innerText = currentQuizData.b;
+    c_text.innerText = currentQuizData.c;
+}
+
+function deselectAnswers(){
+    answerElements.forEach(answerEl => answerEl.checked = false)
+}
+
+function getSelected(){
+    let answer;
+
+    answerElements.forEach(answerEl => {
+        if(answerEl.checked){
+            answer = answerEl.id;
+        }
+    });
+
+    return answer;
+}
+
+submit.addEventListener('click', () => {
+    const answer = getSelected();
+
+    if(answer){
+        if(answer === quizData[currentQuiz].correct){
+            score++;
+        }
+
+        currentQuiz++;
+
+        if(currentQuiz < quizData.length){
+            loadQuiz();
+        }
+        else{
+            quiz.innerHTML = `<h2>You answered coreectly at ${score}/${quizData.length} questions</h2>
+            <button onclick="location.reload()">Reload</button>
+            `;
+        }
+    }
+});
